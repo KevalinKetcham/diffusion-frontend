caf3ae0f0e4ac4c4e0dc3b415d0c91ab2b6c9db9
Kevalin Ketcham <kevalin.create@gmail.com>
2020-11-17 18:18:47 -0800

    Test commit 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test edit
+          test edit 2
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
